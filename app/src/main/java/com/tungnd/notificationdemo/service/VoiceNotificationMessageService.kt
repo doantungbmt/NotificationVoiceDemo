a2269ebb40f91e2a769933d40ca05c3bd4e5ecc1
tungnd <tungnd@namabank.com.vn>
2025-02-28 09:51:17 +0700

    sdfgsr

--- app/src/main/java/com/tungnd/notificationdemo/service/VoiceNotificationMessageService.kt
+++ app/src/main/java/com/tungnd/notificationdemo/service/VoiceNotificationMessageService.kt
@@ -19,22 +19,22 @@ import java.util.Locale
 
 class VoiceNotificationMessageService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        remoteMessage.notification?.let {
-            val messageBody = it.body ?: return
-            Log.d("FCM", "Thông báo nhận được: $messageBody")
-
-            // Gọi hàm phát giọng nói khi nhận thông báo
-            speakText(messageBody)
-        }
-    }
-
-    private fun speakText(message: String) {
-        val intent = Intent(this, TTSForegroundService::class.java).apply {
-            putExtra("message", message)
-        }
-        startService(intent) // Chạy Service phát giọng nói
-    }
+//    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+//        remoteMessage.notification?.let {
+//            val messageBody = it.body ?: return
+//            Log.d("FCM", "Thông báo nhận được: $messageBody")
+//
+//            // Gọi hàm phát giọng nói khi nhận thông báo
+//            speakText(messageBody)
+//        }
+//    }
+//
+//    private fun speakText(message: String) {
+//        val intent = Intent(this, TTSForegroundService::class.java).apply {
+//            putExtra("message", message)
+//        }
+//        startService(intent) // Chạy Service phát giọng nói
+//    }
 
 //    override fun onMessageReceived(remoteMessage: RemoteMessage) {
 //        super.onMessageReceived(remoteMessage)
@@ -72,73 +72,60 @@ class VoiceNotificationMessageService : FirebaseMessagingService() {
 //        notificationManager.notify(0, notificationBuilder.build())
 //    }
 
-//    private lateinit var tts: TextToSpeech
-//    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-//        try {
-//            val channelId = getString(R.string.default_notification_channel_id)
-//
-//            val notificationBuilder = NotificationCompat.Builder(this.applicationContext, channelId)
-//                .setSmallIcon(R.drawable.ic_vietcombank)
-//                .setContentTitle(remoteMessage.notification?.title)
-//                .setContentText(remoteMessage.notification?.body)
-//                .setAutoCancel(true)
-//
-//            val notificationManager =
-//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//                val channel = NotificationChannel(
-//                    channelId,
-//                    getString(R.string.notification_channel),
-//                    NotificationManager.IMPORTANCE_DEFAULT
-//                )
-//                notificationManager.createNotificationChannel(channel)
-//            }
-//            notificationManager.notify(0, notificationBuilder.build())
-//        } catch (e: RemoteException) {
-//            e.printStackTrace()
-//        }
-//        // Check if the message contains notification data
-//        remoteMessage.notification?.let {
-//            val notificationText = it.body
-//            if (notificationText != null) {
-//                // Call the function to convert text to speech
-//                textToSpeech(notificationText)
-//            }
-//        }
-//
-//        if (remoteMessage.data.isNotEmpty()) {
-//            val notificationText = remoteMessage.data["body"]
-//            if (notificationText != null) {
-//                // Start a foreground service to handle text-to-speech in the background
-//                val intent = Intent(this, TTSForegroundService::class.java)
-//                intent.putExtra("text", notificationText)
-//                ContextCompat.startForegroundService(this, intent)
-//            }
-//        }
-//    }
-//
-//    private fun textToSpeech(text: String) {
-//        tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
-//            if (status == TextToSpeech.SUCCESS) {
-//                val result = tts.setLanguage(Locale("vi"))
-//                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-//                    Log.e("TTS", "Language not supported or missing data.")
-//                } else {
-//                    // Speak the text
-//                    tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
-//                }
-//            } else {
-//                Log.e("TTS", "Initialization failed.")
-//            }
-//        })
-//    }
-//
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        // Shutdown TTS when done to release resources
-//        if (::tts.isInitialized) {
-//            tts.stop()
-//            tts.shutdown()
-//        }
-//    }
+    private lateinit var tts: TextToSpeech
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        try {
+            val channelId = getString(R.string.default_notification_channel_id)
+
+            val notificationBuilder = NotificationCompat.Builder(this.applicationContext, channelId)
+                .setSmallIcon(R.drawable.ic_vietcombank)
+                .setContentTitle(remoteMessage.data.get("balance_change_noti"))
+                .setContentText(remoteMessage.notification?.body)
+                .setAutoCancel(true)
+
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    channelId,
+                    getString(R.string.notification_channel),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                notificationManager.createNotificationChannel(channel)
+            }
+            notificationManager.notify(0, notificationBuilder.build())
+        } catch (e: RemoteException) {
+            e.printStackTrace()
+        }
+        remoteMessage.data.let {
+            val str = remoteMessage.data.get("balance_change_noti")
+            Log.d("bodyyyyy", "onMessageReceived: $str" )
+            textToSpeech(remoteMessage.data.get("balance_change_noti").toString())
+        }
+    }
+
+    private fun textToSpeech(text: String) {
+        tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result = tts.setLanguage(Locale("vi"))
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e("TTS", "Language not supported or missing data.")
+                } else {
+                    // Speak the text
+                    tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null)
+                }
+            } else {
+                Log.e("TTS", "Initialization failed.")
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Shutdown TTS when done to release resources
+        if (::tts.isInitialized) {
+            tts.stop()
+            tts.shutdown()
+        }
+    }
 }
